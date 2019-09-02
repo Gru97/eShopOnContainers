30bd47f9ac827f78b86129eff3e2bf912663a169
gru97 <fatemeh.shahidani@gmail.com>
2019-09-02 17:27:56 +0430

    IEventBus and EventBusRabbitMQ added

--- src/BuildingBlocks/EventBus/EventBus/Abstractions/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IEventBus.cs
@@ -8,6 +8,12 @@ namespace EventBus.Abstractions
     public interface IEventBus
     {
         void Publish(IntegrationEvent @event);
-        
+        void Subscribe<TEvent, THandler>()
+            where TEvent : IntegrationEvent
+            where THandler : IIntegrationEventHandler<TEvent>;
+        void Unsubscribe<TEvent, THandler>()
+        where TEvent : IntegrationEvent
+        where THandler : IIntegrationEventHandler<TEvent>;
+
     }
 }
---
+++ src/BuildingBlocks/EventBus/EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,81 @@
+﻿using BuildingBlocks.EventBus.Events;
+using EventBus.Abstractions;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+
+namespace BuildingBlocks.EventBusRabbitMQ
+{
+    //Install-Package RabbitMQ.Client -Version 5.1.0
+    public class EventBusRabbitMQ : IEventBus
+    {
+        private string _brokerName="broker";
+
+        public void Publish(IntegrationEvent @event)
+        {
+            var eventName = @event.GetType().Name;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: _brokerName, type: "direct");
+                    string message = JsonConvert.SerializeObject(@event);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: _brokerName, routingKey: eventName, basicProperties: null, body: body);
+                }
+            }
+            
+        }
+
+        public void Subscribe<TEvent, THandler>()
+            where TEvent : IntegrationEvent
+            where THandler : IIntegrationEventHandler<TEvent>
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: _brokerName, type: "direct");
+
+                string eventName = typeof(TEvent).Name;
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: _brokerName,
+                                  routingKey: eventName);
+
+                
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+               
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                
+            }
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs eventArgs)
+        {
+            var eventName = eventArgs.RoutingKey;
+            var message = Encoding.UTF8.GetString(eventArgs.Body);
+            //process event and call the appropriate handler
+            
+
+           
+        }
+
+        public void Unsubscribe<TEvent, THandler>()
+            where TEvent : IntegrationEvent
+            where THandler : IIntegrationEventHandler<TEvent>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
