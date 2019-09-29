5a2d176491c308fddef58aeacf9fe645fcf4543f
gru97 <fatemeh.shahidani@gmail.com>
2019-09-29 19:51:55 +0330

    Basket Model added

---
+++ src/Services/Basket/Basket.API/Model/CustomerBasket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Model
+{
+    public class CustomerBasket
+    {
+        public List<BasketItem> Items { get; set; }
+        public int CustomerId  { get; set; }
+
+        public CustomerBasket(int Id)
+        {
+            CustomerId = Id;
+            Items=new List<BasketItem>();
+        }
+    }
+}
