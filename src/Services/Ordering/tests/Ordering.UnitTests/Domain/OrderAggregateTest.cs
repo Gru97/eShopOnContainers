b3fcfa743e153e4d3343def4653e4f450461a4e2
gru97 <fatemeh.shahidani@gmail.com>
2019-11-24 23:55:46 +0330

    UnitTest project added for Ordering service

---
+++ src/Services/Ordering/tests/Ordering.UnitTests/Domain/OrderAggregateTest.cs
@@ -0,0 +1,29 @@
+﻿using Ordering.Domain.AggregatesModel.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ordering.UnitTests.Domain
+{
+    public class OrderAggregateTest
+    {
+        [Fact]
+        public void Create_order_Item_success()
+        {
+            //Arrange
+            var productId = 1;
+            var productName = "FakeProductName";
+            var unitPrice = 12;
+            var discount = 10;
+            var units = 5;
+
+            //Act
+            var fakeOrderItem = new OrderItem(units, unitPrice,productId, productName, discount );
+
+            //Assert
+            Assert.NotNull(fakeOrderItem);
+
+        }
+    }
+}
